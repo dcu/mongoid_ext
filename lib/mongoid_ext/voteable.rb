316ebbbed9cf2c543cf3ab4cadc8d331210e082e
David A. Cuadrado <krawek@gmail.com>
2011-06-07 11:33:30 -0500

    modified voting methods to make it more flexible
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/mongoid_ext/voteable.rb
+++ lib/mongoid_ext/voteable.rb
@@ -9,31 +9,31 @@ module MongoidExt
       field :votes, :type => Hash, :default => {}
     end
 
-    def vote!(value, voter, &block)
-      old_vote = self.votes[voter.id]
+    def vote!(value, voter_id, &block)
+      old_vote = self.votes[voter_id]
       if old_vote.nil?
-        self.votes[voter.id] = value
+        self.votes[voter_id] = value
         self.save
-        add_vote!(value, voter, &block)
+        add_vote!(value, voter_id, &block)
         return :created
       else
         if(old_vote != value)
-          self.votes[voter.id] = value
+          self.votes[voter_id] = value
           self.save
-          self.remove_vote!(old_vote, voter, &block)
-          self.add_vote!(value, voter, &block)
+          self.remove_vote!(old_vote, voter_id, &block)
+          self.add_vote!(value, voter_id, &block)
 
           return :updated
         else
-          self.votes.delete(voter.id)
+          self.votes.delete(voter_id)
           self.save
-          remove_vote!(value, voter, &block)
+          remove_vote!(value, voter_id, &block)
           return :destroyed
         end
       end
     end
 
-    def add_vote!(value, voter, &block)
+    def add_vote!(value, voter_id, &block)
       if embedded?
         self._parent.increment({self._position+".votes_count" => 1,
                                 self._position+".votes_average" => value.to_i})
@@ -43,10 +43,10 @@ module MongoidExt
 
       block.call(value, :add) if block
 
-      self.on_add_vote(value, voter) if self.respond_to?(:on_add_vote)
+      self.on_add_vote(value, voter_id) if self.respond_to?(:on_add_vote)
     end
 
-    def remove_vote!(value, voter, &block)
+    def remove_vote!(value, voter_id, &block)
       if embedded?
         self._parent.increment({self._position+".votes_count" => -1,
                                 self._position+".votes_average" => -value.to_i})
@@ -56,7 +56,7 @@ module MongoidExt
 
       block.call(value, :remove) if block
 
-      self.on_remove_vote(value, voter) if self.respond_to?(:on_remove_vote)
+      self.on_remove_vote(value, voter_id) if self.respond_to?(:on_remove_vote)
     end
 
     module ClassMethods
