0f70f5a4b5b0fc043ffb49721a839868ca758e7d
David A. Cuadrado <krawek@gmail.com>
2011-07-03 16:23:55 -0500

    fix issue when max_versions is not given
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- lib/mongoid_ext/versioning.rb
+++ lib/mongoid_ext/versioning.rb
@@ -162,7 +162,7 @@ module Versioning
         uuser_id = send(self.versionable_options[:owner_field]+"_was")||send(self.versionable_options[:owner_field])
         if !self.new? && !data.empty? && uuser_id
           max_versions = self.versionable_options[:max_versions].to_i
-          if self.version_ids.size >= max_versions
+          if max_versions > 0 && self.version_ids.size >= max_versions
             old = self.version_ids.slice!(0, max_versions)
             self.class.skip_callback(:save, :before, :save_version)
             self.version_klass.delete_all(:_ids => old)
